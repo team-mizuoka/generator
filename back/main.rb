d3b22e0b848285d0100df4e4a1e1ddddb238ebd5
mopeneko <mopeneko@lem0n.cc>
2021-07-20 22:59:10 +0900

    Split process

--- back/main.rb
+++ back/main.rb
@@ -6,14 +6,37 @@ require "sinatra/cors"
 require "rmagick"
 require "./uploader.rb"
 
+# CORS
 set :allow_origin, "https://mizuoka-generator.web.app"
 set :allow_methods, "GET,POST"
 set :allow_headers, "content-type"
 set :expose_headers, "location,link"
 
-uploader = CloudStorageUploader.new
+$uploader = CloudStorageUploader.new
 
 post "/generate" do
+  text = prepare_text(params["text"])
+  img = generate(text)
+  file_url = upload_image(img)
+
+  status 500
+  if file_url != ""
+    status 200
+  end
+
+  json :location => file_url
+end
+
+def prepare_text(text)
+  return text
+           .slice(0, 50)
+           .each_char
+           .each_slice(10)
+           .map { |s| s.join }
+           .join("\n")
+end
+
+def generate(text)
   img = Magick::ImageList.new("assets/mizuoka.jpg")
 
   quote = Magick::Draw.new
@@ -22,12 +45,6 @@ post "/generate" do
   height = 0
   pos_x = 205
   pos_y = 145
-  text = params["text"]
-    .slice(0, 50)
-    .each_char
-    .each_slice(10)
-    .map { |s| s.join }
-    .join("\n")
 
   quote.annotate(img, width, height, pos_x, pos_y, text) do
     self.font = "assets/mplus-1p-medium.otf"
@@ -36,6 +53,10 @@ post "/generate" do
     self.fill = "black"
   end
 
+  return img
+end
+
+def upload_image(img)
   file_url = ""
 
   Tempfile.create do |f|
@@ -44,15 +65,10 @@ post "/generate" do
     uuid = SecureRandom.uuid
     output_file_name = "#{uuid}.jpg"
 
-    uploaded_file = uploader.upload(f, output_file_name)
+    uploaded_file = $uploader.upload(f, output_file_name)
 
     file_url = uploaded_file.public_url
   end
 
-  status 500
-  if file_url != ""
-    status 200
-  end
-
-  json :location => file_url
+  return file_url
 end
